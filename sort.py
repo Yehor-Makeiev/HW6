950fc8440b83b4977a0216f2b979c9a1905cdb17
YehorMakeiev <yehor.makeiev@gmail.com>
2023-02-28 14:27:41 +0200

    in progress

--- sort.py
+++ sort.py
@@ -16,7 +16,20 @@ def create_folders(path):
         print(folder)
 
 
+def sort_files(path):
+    for filename in os.listdir(p):
+        if filename.endswith(".txt"):
+            shutil.move(os.path.join(p, filename),
+                        os.path.join(p, 'documents'))
+        if filename.endswith(".bmp"):
+            shutil.move(os.path.join(p, filename),
+                        os.path.join(p, 'images'))
+
+        print(filename)
+
+
 p = r"C:\Users\EgorM\Desktop\hlam"
 
 if __name__ == "__main__":
-    create_folders(p)
+    # create_folders(p)
+    sort_files(p)
