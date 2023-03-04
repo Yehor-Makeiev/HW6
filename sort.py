6829edb78ff25b9a047b5e652b28aa31b4698fcd
YehorMakeiev <yehor.makeiev@gmail.com>
2023-03-04 20:38:08 +0200

    in prodress

--- sort.py
+++ sort.py
@@ -1,35 +1,60 @@
 import os
 from pathlib import Path
+import re
 import shutil
+import glob
+
+folders_dict = {
+    "archives": ['ZIP', 'GZ', 'TAR'],
+    "video": ['AVI', 'MP4', 'MOV', 'MKV'],
+    "audio": ['MP3', 'OGG', 'WAV', 'AMR'],
+    "documents": ['.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx'],
+    "images": ['JPEG', 'PNG', 'JPG', 'SVG']
+}
+
+first_folder = r"C:\Users\EgorM\Desktop\hlam"
+my_path = Path(first_folder)
+
+# Ітеруємось по теці і виводимо всі файли на 1 рівень
 
 
-def create_folders(path):
+def find_and_up(path: Path):
 
-    folders = ["archives", "video", "audio", "documents", "images"]
+    for file_path in path.iterdir():
 
-    # перебираэмо список з назвами тек, будуючи шлях до них і створюючи їх
-    for f in folders:
-        folder = os.path.join(p, f)
-    # перевірка чи існують уже дані теки, якшо існують - не створюємо
-        if not os.path.exists(folder):
-            os.mkdir(folder)
-        print(folder)
+        if file_path.is_dir():
+            find_and_up(file_path)
 
+        elif file_path.is_file():
+            try:
+                file_path.rename(my_path / file_path.name)
+            except FileExistsError:
+                print(
+                    f"{file_path.name} in {file_path.resolve()} - exist, plese rename ")  # додати можливість відразу перейменувати
 
-def sort_files(path):
-    for filename in os.listdir(p):
-        if filename.endswith(".txt"):
-            shutil.move(os.path.join(p, filename),
-                        os.path.join(p, 'documents'))
-        if filename.endswith(".bmp"):
-            shutil.move(os.path.join(p, filename),
-                        os.path.join(p, 'images'))
+# cтворити функцыю для видалення тек!!!!!!
 
-        print(filename)
+# функція для створення тек згідно ключів словника
 
 
-p = r"C:\Users\EgorM\Desktop\hlam"
+def create_folders(path: Path):
+    for key in folders_dict.keys():
+        try:
+            path.joinpath(key).mkdir()
+        except FileExistsError as e:
+            print(e)
 
-if __name__ == "__main__":
-    # create_folders(p)
-    sort_files(p)
+    # сортуємо файли по розширенню зі створенням відповідних тек(згідно словнику)
+for file_path in my_path.iterdir():
+    search = re.findall(r"\.\w+", str(file_path), flags=re.IGNORECASE)
+    try:
+        if search[0] in folders_dict["documents"]:
+            file_path.rename(my_path/"documents"/file_path.name)
+
+    except IndexError as i:
+        print(i)
+
+# find_and_up(my_path)
+
+
+create_folders(my_path)
